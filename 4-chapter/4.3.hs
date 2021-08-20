6d9b3afbc3a2b3b914d1cff20f6b5a5df6fbe7fa
Daniel Cavalcante <danielrcavalcante@gmail.com>
2021-08-19 21:51:21 -0300

    Exercise using rec

--- 4-chapter/4.3.hs
+++ 4-chapter/4.3.hs
@@ -1,2 +1,14 @@
 module Ex3 where
 
+filterOdds:: [Int] -> [Int]
+filterOdds xs = [ x | x <- xs, odd x]
+
+filterEvens:: [Int] -> [Int]
+filterEvens xs = [ x | x <- xs, even x]
+
+filterOddsUsingFilter:: [Int] -> [Int]
+filterOddsUsingFilter xs = filter odd xs
+
+filterEvensUsingFilter:: [Int] -> [Int]
+filterEvensUsingFilter xs = filter even xs
+
---
+++ 4-chapter/4.4.hs
@@ -0,0 +1,18 @@
+module Ex4 where
+import Data.Function
+
+isPrimeNumber:: Int -> Int -> Bool
+isPrimeNumber primeNumber i
+    | primeNumber == 2    = True
+    | primeNumber < 2     = False
+    | mod primeNumber i   == 0 = False 
+    | i * i > primeNumber = True
+    | otherwise = isPrimeNumber primeNumber (i + 1)
+
+handlePrimeNumber :: [Int] -> [Int]
+handlePrimeNumber xs = [x | x <- xs, isPrimeNumber x 2]
+
+filterPrimeNumber:: [Int] -> [Int]
+filterPrimeNumber xs = xs
+    & filter (> 1)
+    & handlePrimeNumber
