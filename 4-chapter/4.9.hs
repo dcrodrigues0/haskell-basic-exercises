482873f35acf7721ea0c409a8e95ee7b79ba9b54
Daniel Cavalcante <danielrcavalcante@gmail.com>
2021-08-22 00:27:18 -0300

    More exercises

--- 4-chapter/4.9.hs
+++ 4-chapter/4.9.hs
@@ -2,6 +2,8 @@ module Ex9 where
 
 import Data.Function ((&))
 
+data Dia = Getsuyoubi | Kayoubi | Suiyobi | Mokuyoubi | Kinyoubi | Doyoubi | Nichiyoubi deriving Show
+
 countNegativeNumbers:: [Int] -> Int
 countNegativeNumbers xs = xs
     & filter (<= 0)
@@ -9,4 +11,26 @@ countNegativeNumbers xs = xs
 
 countPChar:: [Char] -> Int
 countPChar xs = xs
-    & 
+    & filter (== 'P')
+    & (\xs -> [ 1 | x <- xs])
+    & foldl (+) 0
+
+isDoyoubi:: Dia -> Bool
+isDoyoubi Doyoubi = True
+isDoyoubi _       = False
+
+countDoyoubi:: [Dia] -> Int
+countDoyoubi xs = xs
+    & filter isDoyoubi
+    & (\xs -> [1 | x <- xs])
+    & foldl (+) 0
+
+countDays:: [Dia] -> Int
+countDays xs = xs
+    & (\xs -> [1 | x <- xs])
+    & foldl (+) 0
+
+countDaysV2:: [Dia] -> Int -> Int
+countDaysV2 xs count
+    | count == length xs = count
+    | otherwise = countDaysV2 xs $ count + 1
