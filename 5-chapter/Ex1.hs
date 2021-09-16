33e37128fd7cfb25e23c7eca885b4d374ca5bea9
= <=>
2021-09-15 21:57:53 -0300

    fixing a mistake in a exercise

--- 5-chapter/Ex1.hs
+++ 5-chapter/Ex1.hs
@@ -1,7 +1,7 @@
 module Ex1 where
 
 data TypeProduct = Office | Informatics | Book | Movie | Total
-data Product = Product { valor :: Double, tp :: TypeProduct}
+data Product = Product { value :: Double, tp :: TypeProduct} | Nothing 
 
 
 
