9cea70f7e633eb20c0b4475bdd9946c26655c152
= <=>
2021-09-15 21:56:22 -0300

    translating ex1 5 chapter and still trying to understand semigroups monoid

--- 5-chapter/Ex1.hs
+++ 5-chapter/Ex1.hs
@@ -1,4 +1,7 @@
 module Ex1 where
 
-data TipoProduto = Escritorio | Informatica | Livro | Filme | Total
-data Produto = Produto { valor :: Double, tp :: TipoProduto}
+data TypeProduct = Office | Informatics | Book | Movie | Total
+data Product = Product { valor :: Double, tp :: TypeProduct}
+
+
+
