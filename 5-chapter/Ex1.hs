cd131e25f5d87700301e9e7a5088b8f899c7e1e7
= <=>
2021-09-20 18:37:07 -0300

    starting with monoids

--- 5-chapter/Ex1.hs
+++ 5-chapter/Ex1.hs
@@ -1,7 +1,26 @@
 module Ex1 where
+import Data.Monoid
+import Data.Semigroup
 
-data TypeProduct = Office | Informatics | Book | Movie | Total
-data Product = Product { value :: Double, tp :: TypeProduct} | Nothing 
+import Data.Function
 
+data TypeProduct = Office | Informatics | Book | Movie | Total deriving Show
+data ProductEx = ProductEx { value :: Double, tp :: TypeProduct} | NA deriving Show
 
+instance Semigroup ProductEx where
+  (<>) (ProductEx value1 typeProduct1) (ProductEx value2 typeProduct2) 
+    = ProductEx (value1 + value2) Total
 
+instance Monoid ProductEx where
+  mempty = NA
+
+-- Sem usar monoid
+
+getTypeProductValue:: ProductEx -> Double
+getTypeProductValue (ProductEx x _) = x
+
+sumProductValue:: [ProductEx] -> Double
+sumProductValue xs = foldl (+) 0 (getTypeProductValue xs)
+
+-- showProductExTotal::[Double] -> ProductEx
+-- showProductExTotal xs = ProductEx showProductExTotal xs Total
